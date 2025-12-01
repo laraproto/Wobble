3e51a8e30a9e0bba90d84d09c737ed24e78cbac0
laraproto <laraproto@proton.me>
2025-12-01 22:51:19 +0200

    make this not crash without installer configuration, oauth code needs to be reworked for invite codes and to fix bug needing restart after config

--- website/src/modules/oauth/index.ts
+++ website/src/modules/oauth/index.ts
@@ -1,7 +1,10 @@
 import * as arctic from "arctic";
 import { installerConfig } from "../installer";
 
-const redirectUri = new URL("/api/auth/callback", installerConfig?.url);
+const redirectUri = new URL(
+  "/api/auth/callback",
+  installerConfig?.url ?? "http://localhost:3000",
+);
 // we might do something with emails eventually
 export const discordScopes = ["identify", "email"];
 
